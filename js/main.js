8dfacdff600a2392b53c61777ed56af24f534308
Youssef Saad <yousseffhs@yahoo.com>
2019-01-25 23:57:36 -0600

    reseting victory notification

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
 var myArrOfCards = []
-
 $('#gridSelect').change(function () {
     let tx, ty, cardsAvailable;
     let selectedGrid = $(this).val();
-    console.log($(this).val())
     tx = selectedGrid.substr(0, 1)
     ty = selectedGrid.substr(selectedGrid.indexOf('x') + 1)
-    console.log(tx)
-    console.log(ty)
     $('#memory-game').show();
     $('#lobby').hide();
     myArrOfCards = getArrayOfCards(tx, ty);
@@ -30,7 +26,6 @@ function getArrayOfCards(tx, ty) {
         arrOfCards.push(i)
     }
     return shuffleArray(arrOfCards);
-    //return arrOfCards
 }
 
 function shuffleArray(a) {
@@ -42,33 +37,21 @@ function shuffleArray(a) {
 }
 
 function getCard() {
-    console.log('get Currents')
-    console.log(myArrOfCards)
     let item = myArrOfCards[0];
-    console.log('item:', item)
-
-    //
-    console.log(myArrOfCards)
     return item;
 }
 
 function getTableCols(x, currentRow) {
     let cols;
-    console.log(currentRow)
     for (let i = 1; i <= x; i++) {
-        // (${currentRow},${i})- 
         cols += `<td data-row="${i}"><div data-number="${getCard()}"><span>${getCard()}</span></div></td>`
         myArrOfCards.shift();
     }
     return cols;
 }
-
 let turns = [];
 $('#grid table').on('click', 'tr td div', function () {
-    console.log('test 123')
     const dataNumber = $(this).data('number');
-    console.log(dataNumber);
-    console.log(turns.length)
     if (turns.length > 1) {
         $('#result').text('Dont take more than your turn! ')
         turns = []
@@ -77,7 +60,6 @@ $('#grid table').on('click', 'tr td div', function () {
         })
     } else {
         $('#result').text('')
-        console.log(turns.length)
         $(this).children('span').show()
         if (turns.includes(dataNumber)) {
             $('#result').text('Congrats you got that one!!')
@@ -99,16 +81,13 @@ $('#grid table').on('click', 'tr td div', function () {
                 <div class="after"></div>
               </div>You rocked this game!!!`)
             }
-
         } else {
             turns.push(dataNumber)
         }
     }
-
 })
-
-
 $('#back').click(function () {
     $('#memory-game').hide();
     $('#lobby').show()
+    $('#result').html('');
 })
